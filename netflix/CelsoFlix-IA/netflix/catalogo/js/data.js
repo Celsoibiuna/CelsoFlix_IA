a24df7c39570143fefb8ea819a075c5004990478
Celso Batista <tadrika.cs@gmail.com>
2026-04-01 18:33:01 -0300

    Organizando pastas e atualizando links do catalogo

--- netflix/CelsoFlix-IA/netflix/catalogo/js/data.js
+++ netflix/CelsoFlix-IA/netflix/catalogo/js/data.js
@@ -1,6 +1,6 @@
 export const categories = [
     {
-        title: "Épicos",
+        title: "filmes",
         items: [
             {
                 img: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
@@ -15,28 +15,30 @@ export const categories = [
                 youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
             },
             {
-                img: "https://ingresso-a.akamaihd.net/img/cinema/cartaz/14413-destaque.jpg", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=a06zxOyQrAs"
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR99LkAN3DUCf1pO4TC5z0bAAmOTLkqnQrFzQ&s", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=-GY-_4AMN94&t=3s"
             },
         ]
     },
     {
         title: "Séries",
         items: [
-            { img: "https://http2.mlstatic.com/D_NQ_NP_751548-MLA77383606886_072024-C.jpg",
-                 top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { 
+                img: "https://m.media-amazon.com/images/S/pv-target-images/5c3e0d6b9ad0460aa7e63bb1e1d56c4b75722a98f467f5aa8d22650c27a71e1b.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=P_-r11fcDTc&t=3s" 
+            },
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/bb6167717c7a918c1db2bac52234f191024624dcdb41e382a2041959898d789c.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=b_gKwV2ebJY" },
+            { img: "https://i.ytimg.com/vi/Yli24LMMeLc/sddefault.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=yj6bH5uupCo&t=1s" },
+            { img: "https://m.media-amazon.com/images/M/MV5BMDJhODQ3ZDEtN2JmMS00Yjk5LTk1ZTUtMzI3YTU0NTNjMjAyXkEyXkFqcGc@._V1_.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=oT7RNAQVbG4&t=1s" },
         ]
     },
     {
         title: "Para maratonar",
         items: [
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://www.epipoca.com.br/wp-content/uploads/2020/12/Chicago-Fire.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=IIeSDILTE5M" },
+            { img: "https://revistaanamaria.com.br/wp-content/uploads/2024/12/forca-de-mulher.jpg", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=-zj4jAVoI0I&t=1s" },
+            { img: "https://www.ceen.com.br/wp-content/uploads/2024/01/Greys-Anatomy.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=c_9S11SDi14" },
+            { img: "https://upload.wikimedia.org/wikipedia/pt/9/91/Logotipo_de_Os_Dez_Mandamentos.png", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=7XmFHzHuup4&t=2s" },
         ]
     }
 ];
